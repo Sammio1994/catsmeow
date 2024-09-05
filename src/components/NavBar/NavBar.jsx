8271852a77eaec799a319915b1d225b07f983a7d
Zahra Jafaripoor <zahrajafaripoor1394@gmail.com>
2024-09-05 23:27:35 +0100

    Fix navbar styles and layout issues

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,24 +1,41 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css'; // Style it in Navbar.css
+import { FaBars, FaSearch, FaShoppingCart } from 'react-icons/fa'; // Add FontAwesome icons
+
 
 const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
-      <ul className="navbar-list">
-        <li className="navbar-item">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/about">About</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/basket">Basket</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/checkout">Checkout</Link>
-        </li>
-      </ul>
+      <div className="navbar-left">
+        <FaBars className="menu-icon" onClick={toggleMenu} />
+        <ul className={`menu-list ${isMenuOpen ? 'show' : ''}`}>
+          <li className="navbar-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/basket">Basket</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/checkout">Checkout</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <FaSearch className="icon search-icon" />
+        <Link to="/basket">
+          <FaShoppingCart className="icon cart-icon" />
+        </Link>
+      </div>
     </nav>
   );
 };
