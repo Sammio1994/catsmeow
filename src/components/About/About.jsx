a78482779f4a5074713279f9c9f7344bb2ce9d58
Samm Wiltshire <samm94tw@gmail.com>
2024-09-05 11:00:33 +0100

    update

---
+++ src/components/About/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import  img href = https://thecatapi.com/
+
+const About = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default About;
---
+++ src/components/FakeData/FakeData.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FakeData = () => {
+  return (
+    <div>FakeData</div>
+  )
+}
+
+export default FakeData
